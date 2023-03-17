91aff1b35c95a687c8d7445a743b059c791785e0
lana.gordon <lana.gordon@slalom.com>
2023-03-17 09:16:42 +0000

    Refactored code and test

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,9 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Earth")
-	want := "Hello, Earth"
+	t.Run("given a name is supplied, print 'hello name'", func(t *testing.T) {
+		got := Hello("Jake Sully")
+		want := "Hello, Jake Sully"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("given no name is supplied, print 'Hello, Pandora'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, Pandora"
+		assertCorrectMessage(t, got, want)
+	})
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
