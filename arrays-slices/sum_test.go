ddb210d08a56072c1e5d62200a6165e550bdd8e4
lana.gordon <lana.gordon@slalom.com>
2023-06-27 12:38:19 +0100

    Finished array adn slices

--- arrays-slices/sum_test.go
+++ arrays-slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSumArray(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -35,7 +38,39 @@ func TestSumSlice(t *testing.T) {
 
 		if got != want {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
-
 		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v, got, want", got, want)
+		}
+	}
+	t.Run("sum the tail of slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{0, 9, 1})
+		want := []int{5, 10}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("sum the tail an empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9, 1})
+		want := []int{0, 10}
+
+		checkSums(t, got, want)
 	})
 }
